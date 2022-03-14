43c3a405347697e4bfe6ba0cfa85ce27cb795ba1
sathvik848 <99522543+sathvik848@users.noreply.github.com>
2022-03-14 13:08:33 +0530

    Add files via upload

---
+++ aopconcept/src/main/java/com/aop/App.java
@@ -0,0 +1,21 @@
+package com.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aop.services.PaymentService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	
+    	ApplicationContext context=new ClassPathXmlApplicationContext("com/aop/config.xml");
+    	
+    	PaymentService paymentObject = context.getBean("payment",PaymentService.class);
+    	
+    	//Authorize, print:payment started
+    	paymentObject.makePayment();
+    }
+}
---
+++ aopconcept/src/main/java/com/aop/services/PaymentServiceImpl.java
@@ -0,0 +1,17 @@
+package com.aop.services;
+
+public class PaymentServiceImpl implements PaymentService {
+
+	public void makePayment() {
+		
+		//payment code
+		
+		System.out.println("Amount Debited!!!!!");
+		//
+		//
+		//
+		
+		System.out.println("Amount Credited!!!!!");
+	}
+
+}
